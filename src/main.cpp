4c174264eb9e4c7bac6f4e650ccd255adc1e9d1b
Atelsek <120137900+At3lsh3k@users.noreply.github.com>
2025-04-24 15:18:12 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,11 @@
 
 #include <Arduino.h>
 
-#define nizka 10
-#define visoka 11
+
 #define senzor A0
 
 void setup() {
-  pinMode(nizka, OUTPUT);
-  pinMode(visoka, OUTPUT);
+
   
   Serial.begin(9600);
  
@@ -20,8 +18,7 @@ void loop() {
   int vrednost = analogRead(senzor);
   float tlakBar = ((6./ 790.) * vrednost); 
 
-  // 97 bitov v luftu
-  // 785 bitov v 6
+ 
 
   
     Serial.print(tlakBar);
@@ -30,17 +27,7 @@ void loop() {
     Serial.println(" bit");
     delay(500);
     
-    if (tlakBar < 0.8){
-      digitalWrite(nizka, HIGH);
-      digitalWrite(visoka, LOW);
-    } else if (tlakBar > 9.83) {
-      digitalWrite(nizka, LOW);
-      digitalWrite(visoka, HIGH);
-    } else {
-      digitalWrite(visoka, LOW);
-      digitalWrite(nizka, LOW);
-    }
-  
+
     
   }
   
